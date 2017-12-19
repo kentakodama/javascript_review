bd733e33968ffaacfc233ac12445106b5f9dbeb7
Kenta Kodama <kentakodama@gmail.com>
2017-12-18 21:45:54 -0800

    added instructions for part three

---
+++ w5d3/skeleton/phase_3_iteration.js
@@ -0,0 +1,2 @@
+// Array#bubbleSort - receives an array, returns a sorted array by implementing bubble sort sorting algorithm
+// String#substrings - receives a string, returns an array of all substrings
