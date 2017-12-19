f3c04180f42e3a05352a017f7c7bfb11befa0a12
Kenta Kodama <kentakodama@gmail.com>
2017-12-18 20:48:47 -0800

    added transposed funciton

--- w5d3/skeleton/phase_1_arrays.js
+++ w5d3/skeleton/phase_1_arrays.js
@@ -1,13 +1,13 @@
-Monkey-patch the following methods to the Array class. Remember, we want to use prototype here.
-
-Array#uniq - returns a new array containing only the unique elements of the original array
-the unique elements should be in the order in which they first appear
-should not mutate the original array
-
-Array#twoSum - returns an array of position pairs where the elements sum to zero
-
-Array#transpose - where we have a two-dimensional array representing a matrix. returns the transpose
-should not mutate the original array
+// Monkey-patch the following methods to the Array class. Remember, we want to use prototype here.
+// 
+// Array#uniq - returns a new array containing only the unique elements of the original array
+// the unique elements should be in the order in which they first appear
+// should not mutate the original array
+// 
+// Array#twoSum - returns an array of position pairs where the elements sum to zero
+// 
+// Array#transpose - where we have a two-dimensional array representing a matrix. returns the transpose
+// should not mutate the original array
 
 
 Array.prototype.uniq = function () {
@@ -26,11 +26,40 @@ Array.prototype.uniq = function () {
 Array.prototype.twoSum = function () {
     let positions = [];
     for (var i = 0; i < this.length; i++) {
-      for (var j = 0; j < this.length; j++) {
+      for (var j = i + 1; j < this.length; j++) {
         if (this[i] + this[j] === 0) {
-          positons.push([i, j]);
+          positions.push([i, j]);
         }
       }
     }
     return positions;
 };
+
+
+Array.prototype.transpose = function () {
+  
+  let reversed = []
+  
+  let i = 0
+  while(i < this[0].length) {
+    reversed.push([])
+  }
+  
+  let y = 0 
+  while (y < this.length) {
+    let x = 0
+    while (x < this[y].length) {
+      reversed[x][y] = this[y][x];
+      x ++;
+    }
+    y ++;
+  }
+  return reversed
+};
+
+
+
+
+
+
+
