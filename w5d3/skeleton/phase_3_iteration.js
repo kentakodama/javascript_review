a4e29c406113a2f2624f1664475ca71c19bcb180
Kenta Kodama <kentakodama@gmail.com>
2017-12-18 21:54:30 -0800

    destructuring arrays\

--- w5d3/skeleton/phase_3_iteration.js
+++ w5d3/skeleton/phase_3_iteration.js
@@ -1,2 +1,21 @@
 // Array#bubbleSort - receives an array, returns a sorted array by implementing bubble sort sorting algorithm
 // String#substrings - receives a string, returns an array of all substrings
+
+Array.prototype.bubbleSort = function () {
+  
+  let i = 0;
+  while(i < this.length) {
+    let j = i + 1
+    while(j < this.length) {
+      if (this[i] > this[j]) {
+        [this[i], this[j]] = [this[j], this[i]]
+        //destructuring
+      }
+      j ++;
+    }
+    i ++;
+  }
+  
+  return this
+  
+};
