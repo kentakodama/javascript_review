697255e77344d26533eedb3e8d7073fe40d4a4ba
Kenta Kodama <kentakodama@gmail.com>
2017-12-19 17:07:41 -0800

    added rest and spread operator examples

--- w5d4/arguments.js
+++ w5d4/arguments.js
@@ -1,5 +1,5 @@
 
-
+ES5 
 NB: There is a special arguments array that exists for every function. 
 It can only take three methods: length, indexed [], and slice
 
@@ -11,3 +11,26 @@ function thisWorks() {
   args.forEach((arg)=> console.log(arg)); // This works!
 }
 
+ES6 
+For creating functions 
+//Rest operator ... will grab all other args and store them in a real array 
+function restWay(firstArg, ...otherArgs) {
+  console.log(`The first arg is ${firstArg}!`);
+
+  console.log(`The other args are:`);
+
+  otherArgs.forEach((arg) => {
+    console.log(arg);
+  });
+}
+
+For invoking functions 
+//the spread operator can destructure an array so that a function can take invidiual args 
+function madLib(verb, pluralNoun1, pluralNoun2, place) {
+  return `I like to ${verb} ${pluralNoun1} with ${pluralNoun2} by the ${place}.`;
+}
+
+const words = ["eat", "socks", "rabbits", "sea"];
+
+madLib(...words);
+//here
