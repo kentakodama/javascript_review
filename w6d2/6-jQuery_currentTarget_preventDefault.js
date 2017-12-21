8075983b33468a930e40935109f3916c48316e06
Kenta Kodama <kentakodama@gmail.com>
2017-12-20 17:20:18 -0800

    preventDefault explanation added

--- w6d2/6-jQuery_currentTarget_preventDefault.js
+++ w6d2/6-jQuery_currentTarget_preventDefault.js
@@ -1,5 +1,5 @@
 
-//currentTarget 
+currentTarget 
 // a method that identifies which specific element is triggering the event.
 // Useful because: if same event handler is on all list items, we need to know
 // which specific item triggered it so we can do something with it.
@@ -18,3 +18,9 @@
     alert($currentTarget.text());
   })
 </script>
+
+preventDefault
+//We have also discussed previously about the need to preventDefault. 
+//This stops the event from triggering any built-in actions. 
+//This is most important for form submit events, since if the event is not canceled by your JavaScript code, the browser will perform the default action, which is to make an HTTP request submitting a form.
+
